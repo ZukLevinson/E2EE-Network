13d0a4abec7acd0beca4c45d5ab7fcaa2ea8be41
Zuk Levinson <zuklevinson@gmail.com>
2022-07-29 21:02:24 +0300

    feat(side-bar.component): create component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SideBarComponent
+    SideBarComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
